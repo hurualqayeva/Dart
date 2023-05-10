d9030a92c46b3e12a9dcc93a7b854d7ef10ea46d
Huru <hurualqayeva@gmail.com>
2023-05-11 02:11:21 +0400

    fun

--- Lesson_5/passwordGenerator.dart
+++ Lesson_5/passwordGenerator.dart
@@ -3,12 +3,6 @@ import "dart:math";
 import 'dart:convert';
 
 main() {
-//   Random()
-// List.generate()
-// nextInt()
-// base64UrlEncode()
-// shuffle()
-
   print("3 seçimdən birini daxil etməyiniz xahiş edilir:zəif, orta , güclü");
   String choice = stdin.readLineSync().toString().toLowerCase();
   PasswordGenerator(choice);
@@ -22,9 +16,9 @@ void PasswordGenerator(String choice) {
         List<int>.generate(5, (intValue) => intValue * 3, growable: true);
     growableList.shuffle();
     List<String> c = base64UrlEncode(growableList)
-        .split(""); // ['a', 'a', 'a', 'b', 'c', 'd']
-    c.removeLast(); // ['a', 'a', 'a', 'b', 'c']
-    print(c.join().substring(0,5)); //aaabc
+        .split(""); 
+    c.removeLast(); 
+    print(c.join().substring(0,5)); 
   } else if (choice.startsWith("o")) {
     int intValue = Random().nextInt(10);
     String base64UrlEncode(List<int> bytes) => base64Url.encode(bytes);
@@ -32,9 +26,9 @@ void PasswordGenerator(String choice) {
         List<int>.generate(8, (intValue) => intValue * 3, growable: true);
     growableList.shuffle();
     List<String> c = base64UrlEncode(growableList)
-        .split(""); // ['a', 'a', 'a', 'b', 'c', 'd']
-    c.removeLast(); // ['a', 'a', 'a', 'b', 'c']
-    print(c.join().substring(0,8)); //aaabc
+        .split(""); 
+    c.removeLast(); 
+    print(c.join().substring(0,8)); 
   } else if (choice.startsWith("g")) {
     int intValue = Random().nextInt(10);
     String base64UrlEncode(List<int> bytes) => base64Url.encode(bytes);
@@ -42,8 +36,8 @@ void PasswordGenerator(String choice) {
         List<int>.generate(10, (intValue) => intValue * 3, growable: true);
     growableList.shuffle();
     List<String> c = base64UrlEncode(growableList)
-        .split(""); // ['a', 'a', 'a', 'b', 'c', 'd']
-    c.removeLast(); // ['a', 'a', 'a', 'b', 'c']
+        .split(""); 
+    c.removeLast();
     print(c.join().substring(0,10));
   } else {
     print("Düzgün daxil edin.");
